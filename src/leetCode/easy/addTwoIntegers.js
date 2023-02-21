beac539f750200abafa5fd93df3d1952c2c743ee
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-02-21 19:35:32 +0200

    addTwoIntegers soluation

---
+++ src/leetCode/easy/addTwoIntegers.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log('case 1', sum(10, 5));
+console.log('##########################');
+console.log('case 2', sum(-6, -2));
+console.log('##########################');
---
+++ src/leetCode/easy/addTwoIntegers.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+console.log('case 1', sum(10, 5));
+console.log('##########################');
+console.log('case 2', sum(-6, -2));
+console.log('##########################');
