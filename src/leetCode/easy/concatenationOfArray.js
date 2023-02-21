d7f676b64d7be5b936224446262594fea7166dd4
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-02-21 19:28:47 +0200

    concatenationOfArray soluation

---
+++ src/leetCode/easy/concatenationOfArray.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+//pseudocode
+//first --> store arr of nums in another variable
+//second --> concat arr1 in arr2
+function getConcatenation(nums) {
+    var ans = nums;
+    return ans.concat(nums);
+}
+console.log('case 1', getConcatenation([1, 2, 3, 6]));
+console.log('##########################');
+console.log('case 2', getConcatenation([1, 2, 3, 6, 7, 8, 9, 10]));
+console.log('##########################');
+console.log('case 3', getConcatenation([1, 12, 53, 36, 55, 89, 88]));
